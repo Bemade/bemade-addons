90dedc5b2ce484544f83c4371a5ba90fc5931f07
Marc Durepos <marc@bemade.org>
2023-06-12 11:33:30 -0400

    bemade_fsm: Update to manifest and gitignore.

--- __manifest__.py
+++ __manifest__.py
@@ -1,3 +1,23 @@
+######################################################################################
+#
+#    Bemade Inc.
+#
+#    Copyright (C) 2023-June Bemade Inc. (<https://www.bemade.org>).
+#    Author: Marc Durepos (Contact : mdurepos@durpro.com)
+#
+#    This program is under the terms of the Odoo Proprietary License v1.0 (OPL-1)
+#    It is forbidden to publish, distribute, sublicense, or sell copies of the Software
+#    or modified copies of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+#    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#    DEALINGS IN THE SOFTWARE.
+#
+########################################################################################
 {
     'name': 'Durpro Field Service Management',
     'version': '15.0.1.0.0',
@@ -6,7 +26,7 @@
     'category': 'Services/Field Service',
     'author': 'Bemade Inc.',
     'website': 'http://www.bemade.org',
-    'license': 'LGPL-3',
+    'license': 'OPL-1',
     'depends': ['project', 'stock', 'sale', 'sale_project', 'sale_stock'],
     'data': ['views/task_template_views.xml',
              'security/ir.model.access.csv',
