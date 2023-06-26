f0da5c361818a29c3f2d12a11f0ad44d1ddb0840
Marc Durepos <marc@bemade.org>
2023-06-26 15:01:17 -0400

    bemade_partner_root_ancestor: Added store=true, recursive=true to root ancestor field.
    bemade_partner_root_ancestor: Preparing to migrate bemade_partner_root_ancestor.

--- bemade_partner_root_ancestor/models/res_partner.py
+++ bemade_partner_root_ancestor/models/res_partner.py
@@ -6,9 +6,11 @@ class Partner(models.Model):
 
     root_ancestor = fields.Many2one(comodel_name='res.partner',
                                     string='Root Ancestor',
-                                    compute='_compute_root_ancestor')
+                                    compute='_compute_root_ancestor',
+                                    store=True,
+                                    recursive=True)
 
-    @api.depends('parent_id')
+    @api.depends('parent_id', 'parent_id.root_ancestor')
     def _compute_root_ancestor(self):
         for rec in self:
             rec.root_ancestor = rec.parent_id and rec.parent_id.root_ancestor or rec
