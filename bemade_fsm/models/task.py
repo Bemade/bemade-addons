653e22d13c3f7274e9620f33531e68db03514803
Marc Durepos <marc@bemade.org>
2023-06-27 16:18:12 -0400

    Refactoring to views to reuse trees for fsm contacts and equipment. Began remodeling equipment/task relationship (breaks tests).

--- bemade_fsm/models/task.py
+++ bemade_fsm/models/task.py
@@ -4,8 +4,24 @@ from odoo import fields, models, api
 class Task(models.Model):
     _inherit = "project.task"
 
-    equipment_id = fields.Many2one("bemade_fsm.equipment", string="Target Equipment")
-
-    work_order_contacts = fields.Many2many(related='sale_line_id.order_id.work_order_contacts')
-    site_contacts = fields.Many2many(related='sale_line_id.order_id.site_contacts')
-    equipment_ids = fields.Many2many(related='sale_line_id.order_id.equipment_ids')
+    work_order_contacts = fields.Many2many(comodel_name="res.partner",
+                                           relation="task_work_order_contact_rel",
+                                           column1="task_id",
+                                           column2="res_partner_id",
+                                           compute="_compute_contacts",
+                                           inverse="_inverse_contacts",
+                                           store=True)
+    site_contacts = fields.Many2many(comodel_name="res.partner",
+                                           relation="task_site_contact_rel",
+                                           column1="task_id",
+                                           column2="res_partner_id",
+                                           compute="_compute_contacts",
+                                           inverse="_inverse_contacts",
+                                           store=True)
+    equipment_ids = fields.Many2many(comodel_name="bemade_fsm.equipment",
+                                     relation="task_equipment_rel",
+                                     column1="task_id",
+                                     column2="res_partner_id",
+                                     compute="_compute_equipment",
+                                     inverse="_inverse_equipment",
+                                     store=True)
