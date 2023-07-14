4010fd57a28bb9921d7e97a27af04e57256f6e48
Marc Durepos <marc@bemade.org>
2023-07-13 21:42:16 -0400

    bemade_fsm: major refactoring of tests in progress (broken).

--- bemade_fsm/tests/test_equipment.py
+++ bemade_fsm/tests/test_equipment.py
@@ -1,53 +1,34 @@
 from odoo.tests.common import HttpCase, tagged
-from .test_bemade_fsm_common import FSMManagerUserTransactionCase
+from .test_bemade_fsm_common import BemadeFSMBaseTest
 from odoo import Command
 from odoo.exceptions import MissingError
 
 
 @tagged("-at_install", "post_install")
-class TestEquipmentCommon(FSMManagerUserTransactionCase):
+class TestEquipment(BemadeFSMBaseTest):
+    def test_crud(self):
+        self.partner_company = self._generate_partner()
+        self.partner_contact = self._generate_partner('Site Contact', 'person', self.partner_company)
+        self.equipment = self._generate_equipment('Test Equipment 1', self.partner_company)
 
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-        # Set up the test partner
-        cls.partner_company = cls.env['res.partner'].create({
-            'name': 'Test Partner Company',
-            'company_type': 'company',
-            'street': '123 Street St.',
-            'city': 'Montreal',
-            'state_id': cls.env['res.country.state'].search([('name', 'ilike', 'Quebec%')]).id,
-            'country_id': cls.env['res.country'].search([('name', '=', 'Canada')]).id
-        })
-
-        cls.partner_contact = cls.env['res.partner'].create({
-            'name': 'Site Contact',
-            'company_type': 'person',
-            'parent_id': cls.partner_company.id,
-        })
-
-        cls.equipment = cls.env['bemade_fsm.equipment'].create({
-            'name': 'Test Equipment 1',
-            'partner_location_id': cls.partner_company.id,
-        })
-
-
-@tagged('-at_install', 'post_install')
-class TestEquipmentBase(TestEquipmentCommon):
-
-    def test_crd(self):
         # Just make sure the basic ORM stuff is OK
         self.assertTrue(self.equipment in self.partner_company.equipment_ids)
         self.assertTrue(len(self.partner_company.equipment_ids) == 1)
-        self.partner_company.write({'equipment_ids': [Command.set([])]})
+
         # Delete should cascade
+        self.partner_company.write({'equipment_ids': [Command.set([])]})
         with self.assertRaises(MissingError):
             self.equipment.name
 
 
 @tagged('-at_install', 'post_install', 'slow')
-class TestEquipmentTours(HttpCase, TestEquipmentCommon):
+class TestEquipmentTours(HttpCase, BemadeFSMBaseTest):
+
+    @classmethod
+    def setUpClass(cls):
+        cls._generate_partner()
+        cls._generate_partner('Site Contact', 'person', cls.partner_company)
+        cls._generate_equipment('Test Equipment 1', cls.partner_company)
 
     def test_equipment_base_tour(self):
         self.start_tour('/web', 'equipment_base_tour',
