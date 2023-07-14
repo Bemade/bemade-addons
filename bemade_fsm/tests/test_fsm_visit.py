3771c368996c0fe966b5b17ab6e6c6f6d4604e3c
Marc Durepos <marc@bemade.org>
2023-07-14 13:39:30 -0400

    bemade_fsm: major refactoring of unit tests completed. Integration tests need rework.

--- bemade_fsm/tests/test_fsm_visit.py
+++ bemade_fsm/tests/test_fsm_visit.py
@@ -1,24 +1,22 @@
 from odoo.tests import TransactionCase, tagged, Form
+from .test_bemade_fsm_common import BemadeFSMBaseTest
 
 
 @tagged('-at_install', 'post_install')
-class FSMVisitTest(TransactionCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
+class FSMVisitTest(BemadeFSMBaseTest):
 
     def test_create_visit_sets_name_on_section(self):
-        so = self.generate_sale_order()
-        self.add_service_so_line(so)
+        so = self._generate_sale_order()
+        self._add_service_so_line(so)
 
-        visit = self.generate_visit(so)
+        visit = self._generate_visit(so)
 
         self.assertTrue(visit.so_section_id)
         self.assertEqual(visit.so_section_id.name, visit.label)
 
     def test_change_visit_section_name(self):
-        so = self.generate_sale_order()
-        visit = self.generate_visit(so, label="First Label")
+        so = self._generate_sale_order()
+        visit = self._generate_visit(so, label="First Label")
         line = visit.so_section_id
 
         line.name = "Second Label"
@@ -26,8 +24,8 @@ class FSMVisitTest(TransactionCase):
         self.assertEqual(visit.label, "Second Label")
 
     def test_change_visit_label_changes_section_name(self):
-        so = self.generate_sale_order()
-        visit = self.generate_visit(so, label="First Label")
+        so = self._generate_sale_order()
+        visit = self._generate_visit(so, label="First Label")
         line = visit.so_section_id
 
         visit.label = "Second Label"
@@ -35,9 +33,9 @@ class FSMVisitTest(TransactionCase):
         self.assertEqual(line.name, "Second Label")
 
     def test_visit_completes_when_task_completes(self):
-        so = self.generate_sale_order()
-        visit = self.generate_visit(so)
-        sol = self.add_service_so_line(so, task=True)
+        so = self._generate_sale_order()
+        visit = self._generate_visit(so)
+        self._add_service_so_line(so, task=True)
         so.action_confirm()
         task = so.order_line.filtered(lambda l: l.task_id).task_id
 
@@ -46,65 +44,36 @@ class FSMVisitTest(TransactionCase):
         self.assertTrue(visit.is_completed)
 
     def test_visit_shows_invoiced_when_invoiced(self):
-        so = self.generate_sale_order()
-        visit = self.generate_visit(so)
-        self.add_service_so_line(so, task=True)
+        so = self._generate_sale_order()
+        visit = self._generate_visit(so)
+        self._add_service_so_line(so, task=True)
         so.action_confirm()
         task = so.order_line.filtered(lambda l: l.task_id).task_id
         task.action_fsm_validate()
 
-        self.invoice_sale_order(so)
+        self._invoice_sale_order(so)
 
         self.assertTrue(visit.is_invoiced)
 
-    def invoice_sale_order(self, so):
+    def _invoice_sale_order(self, so):
         wiz = self.env['sale.advance.payment.inv'].with_context({'active_ids': [so.id]}).create({})
         wiz.create_invoices()
         inv = so.invoice_ids[-1]
         inv.action_post()
         return inv
 
-    def generate_visit(self, sale_order, label="Test Label"):
+    def _generate_visit(self, sale_order, label="Test Label"):
         return self.env['bemade_fsm.visit'].create([{
             'sale_order_id': sale_order.id,
             'label': label,
         }])
 
-    def generate_sale_order(self, partner_id=None):
-        so = self.env['sale.order'].create({
-            'partner_id': partner_id or self.generate_partner().id,
-            'client_order_ref': 'Test',
-        })
-        return so
-
-    def add_service_so_line(self, sale_order, task: bool = False):
+    def _add_service_so_line(self, sale_order, task: bool = False):
         """ Generates a sales order line for a service product.
 
         :param sale_order: The sales order to which the new line is to be added
         :param task: If true, the created line will be for a product with service_tracking=task_global_project
         """
-        if not task:
-            service_tracking = 'no'
-            project = False
-        else:
-            project = self.env.ref("industry_fsm.fsm_project")
-            service_tracking = 'task_global_project'
-        product = self.env['product.product'].create({
-            'name': 'Test Product',
-            'type': 'service',
-            'service_tracking': service_tracking,
-            'project_id': project and project.id,
-            'price': 100.0,
-            'sale_ok': True,
-        })
-        return self.env['sale.order.line'].create({
-            'product_id': product.id,
-            'order_id': sale_order.id,
-        })
-
-    def generate_partner(self):
-        partner = self.env['res.partner'].create({
-            'name': 'Test Partner',
-            'company_type': 'company',
-        })
-        return partner
+        service_tracking = 'task_global_project' if task else 'no'
+        product = self._generate_product(service_tracking=service_tracking)
+        return self._generate_sale_order_line(sale_order, product)
