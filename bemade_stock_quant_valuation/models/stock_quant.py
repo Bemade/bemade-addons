538aac89c3e663b8b1933887ef2d78c09c3ea584
Marc Durepos <marc@bemade.org>
2023-09-19 15:09:10 -0400

    bemade_stock_quant_valuation new module to add valuation on the inventory adjustment view.

---
+++ bemade_stock_quant_valuation/models/stock_quant.py
@@ -0,0 +1,21 @@
+from odoo import models, fields, api
+
+
+class StockQuant(models.Model):
+    _inherit = 'stock.quant'
+
+    value_unit = fields.Float(related='product_id.standard_price', readonly=True,
+                              groups='stock.group_stock_manager', digits='Product Price')
+    value_difference = fields.Float(compute='_compute_difference_value',
+                                    help='The value of the difference between the '
+                                         'quantity on hand and the counted '
+                                         'quantity.',
+                                    groups='stock.group_stock_manager',
+                                    digits='Product Price',
+                                    store=True)
+
+    @api.depends('inventory_diff_quantity', 'value_unit')
+    def _compute_difference_value(self):
+        for rec in self:
+            rec.value_difference = (rec.value_unit * rec.inventory_diff_quantity
+                                    if rec.inventory_diff_quantity is not None else None)
