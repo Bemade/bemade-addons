e7a360c2cabdde1749d81be7b2d9064a85ed54e0
Benoît Vézina <benoit@vezina.biz>
2023-10-24 17:04:33 -0400

    Update res_partner.py

--- bemade_sethy_configuration/model/res_partner.py
+++ bemade_sethy_configuration/model/res_partner.py
@@ -1,1 +1,13 @@
+# -*- coding: utf-8 -*-
 
+from odoo import api, fields, models, _
+from odoo.addons.website.models import ir_http
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    surface = fields.Float(string='Surface')
+    ref_project = fields.Text(string='Project')
+    intent = fields.Bollean(string='Intent')
+    
