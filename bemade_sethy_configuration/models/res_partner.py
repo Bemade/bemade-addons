8cf9af4e266b1f42557fe6e0a83291f483dd19e3
Benoît Vézina <benoit@vezina.biz>
2023-10-25 10:54:39 -0400

    mercredi

--- bemade_sethy_configuration/models/res_partner.py
+++ bemade_sethy_configuration/models/res_partner.py
@@ -7,7 +7,17 @@ from odoo.addons.website.models import ir_http
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    surface = fields.Float(string='Surface')
-    ref_project = fields.Text(string='Project')
-    intent = fields.Boolean(string='Intent')
-    
+    surface = fields.Float(string='Surface (ha)')
+    lot_number = fields.Text(string='Lot Number')
+    ref_project = fields.Text(string='Project No')
+    intent_signing = fields.Boolean(string='Intent to sign')
+    cyberimpact = fields.Boolean(string='Cyberimpact')
+    specification_date = fields.Date(string='Specification date')
+    crm_stage_activity = fields.Text(string='Project No')
+    interest_level = fields.Selection(selection=[('1','None'),
+                                                 ('2','Low'),
+                                                 ('3','Average'),
+                                                 ('4','High')], string='Interest Level')
+    company_use = fields.Text(string='Company')
+
+
