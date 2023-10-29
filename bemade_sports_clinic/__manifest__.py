59b0e2b0998f47c51376775b76aee113aa6c55a4
Marc Durepos <marc@bemade.org>
2023-10-29 16:13:11 -0400

    Version bump and icon update for bemade_sports_clinic.

--- bemade_sports_clinic/__manifest__.py
+++ bemade_sports_clinic/__manifest__.py
@@ -18,7 +18,7 @@
 #
 {
     'name': 'Sports Clinic Management',
-    'version': '16.0.1.1.0',
+    'version': '16.0.1.3.0',
     'summary': 'Manage the patients of a sports medicine clinic.',
     'description': """
         Adds the notion of sports teams, players (patients), coaches and treatment
