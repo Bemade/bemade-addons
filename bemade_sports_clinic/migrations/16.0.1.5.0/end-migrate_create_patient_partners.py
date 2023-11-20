4fb327a7053e080c97840e69f6c4c8031124fe9d
Marc Durepos <marc@bemade.org>
2023-11-20 15:53:14 -0500

    Added migration script to move to 16.0.1.5.0 for sports clinic module.

---
+++ bemade_sports_clinic/migrations/16.0.1.5.0/end-migrate_create_patient_partners.py
@@ -0,0 +1,10 @@
+from odoo import api, SUPERUSER_ID
+
+
+def migrate(cr, version):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    patients = env['sports.patient'].search([('partner_id', '=', False)])
+    for patient in patients:
+        env['res.partner'].create({
+            'name': patient._get_name_from_first_and_last(patient.first_name, patient.last_name)
+        })
