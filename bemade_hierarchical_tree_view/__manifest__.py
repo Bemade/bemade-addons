8cf4d716b38d46376c5e2994d5f4f42e84dd0277
Marc Durepos <marc@bemade.org>
2023-12-12 14:49:53 -0500

    first branch commit for bemade_hierarchical_tree_view.

---
+++ bemade_hierarchical_tree_view/__manifest__.py
@@ -0,0 +1,36 @@
+#
+#    Bemade Inc.
+#
+#    Copyright (C) 2023-June Bemade Inc. (<https://www.bemade.org>).
+#    Author: Marc Durepos (Contact : mdurepos@durpro.com)
+#
+#    This program is under the terms of the Odoo Proprietary License v1.0 (OPL-1)
+#    It is forbidden to publish, distribute, sublicense, or sell copies of the Software
+#    or modified copies of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+#    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#    DEALINGS IN THE SOFTWARE.
+#
+{
+    'name': 'Hierarchical Tree View',
+    'version': '16.0.1.0.0',
+    'summary': 'Creates a new view type "htree" to be used when defining views',
+    'category': 'Technical',
+    'author': 'Bemade Inc.',
+    'website': 'http://www.bemade.org',
+    'license': 'OPL-1',
+    'depends': ['web'],
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            'bemade_hierarchical_tree_view/src/*/**',
+        ]
+    },
+    'installable': True,
+    'auto_install': False
+}
---
+++ bemade_hierarchical_tree_view/src/components/hierarchical_tree_view_controller.js
@@ -0,0 +1,8 @@
+/** @odoo-module **/
+
+import { ListController } from "@web/views/list/list_controller";
+import { ListRenderer } from "@web/views/list/list_renderer";
+
+export class HierarchicalTreeViewController extends ListController {
+
+}
