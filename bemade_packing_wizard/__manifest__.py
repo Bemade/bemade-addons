c8dc89cf29e3f912d77dd24eb001b0496bb1e6ad
Marc Durepos <marc@bemade.org>
2024-02-05 11:48:47 -0500

    bemade_packing_wizard: bug fix on package creation

--- bemade_packing_wizard/__manifest__.py
+++ bemade_packing_wizard/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Packing wizard',
-    'version': '15.0.1.0.0',
+    'version': '15.0.1.0.1',
     'category': 'Extra Tools',
     'summary': 'Allow automated packing type creation',
     'description': """
